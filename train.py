8fef6a6b9664a7759a0aace4efbdd4369fca6eee
weiaicunzai <372995411@qq.com>
2019-01-03 00:04:32 +0800

    [save work]

--- train.py
+++ train.py
@@ -12,43 +12,66 @@ from transforms import transforms
 
 path = '/Users/didi/Downloads/train/2d281959a02178bbcdeea424c8757b1d.jpg'
 
+#data_r = np.array([])
+#data_g = np.array([])
+#data_b = np.array([])
+data_r = []
+data_g = []
+data_b = []
+
 for i in glob.iglob('/Users/didi/Downloads/train/*.jpg'):
     image_cv = cv2.imread(i)
-    #b, g, r = cv2.split(image_cv)
+    b, g, r = cv2.split(image_cv)
+    data_b.append(b.flatten())
+    data_r.append(r.flatten())
+    data_g.append(g.flatten())
+    
+print('fff')
+data_b = np.hstack(data_b)
+data_r = np.hstack(data_r)
+data_g = np.hstack(data_g)
+print('fff')
+#print('b')
+#data_g = np.hstack([cv2.imread(i)[:, :, 1].flatten() for i in glob.iglob('/Users/didi/Downloads/train/*.jpg')])
+#print('g')
+#data_r = np.hstack([cv2.imread(i)[:, :, 2].flatten() for i in glob.iglob('/Users/didi/Downloads/train/*.jpg')])
+#print('r')
+print(np.mean(data_b) / 255.0, np.mean(data_g) / 255.0, np.mean(data_r) / 255.0)
+print(np.std(data_b) / 255.0, np.std(data_g) / 255.0, np.std(data_r) / 255.0)
     #image_cv = cv2.merge((r, g, b))
 
 
-    image_cv = cv2.resize(image_cv, (224, 224), interpolation=cv2.INTER_LINEAR)
-    print(image_cv.dtype)
-
-    image_pil = Image.open(i)
-    image_pil = image_pil.resize((224, 224), resample=Image.BILINEAR)
-
+    #image_cv = cv2.resize(image_cv, (224, 224), interpolation=cv2.INTER_LINEAR)
     #print(image_cv.dtype)
+
+    #image_pil = Image.open(i)
+    #image_pil = image_pil.resize((224, 224), resample=Image.BILINEAR)
+
+    ##print(image_cv.dtype)
+    ##print(image_pil.dtype)
+
+    #image_pil.show('pil')
+
+    #image_pil = np.array(image_pil)
     #print(image_pil.dtype)
 
-    image_pil.show('pil')
+    #r, g, b = cv2.split(image_pil)
+    #image_pil = cv2.merge((b, g, r))
 
-    image_pil = np.array(image_pil)
-    print(image_pil.dtype)
+    #print(np.mean(image_cv - image_pil))
 
-    r, g, b = cv2.split(image_pil)
-    image_pil = cv2.merge((b, g, r))
+    #diff = image_cv - image_pil
+    #print('diff ', np.max(diff))
+    #print('diff ', np.min(diff))
+    #diff = diff + - np.min(diff)
+    #diff = diff / np.max(diff) * 255
 
-    print(np.mean(image_cv - image_pil))
+    #print('diff ', np.max(diff))
+    #print('diff ', np.min(diff))
 
-    diff = image_cv - image_pil
-    print('diff ', np.max(diff))
-    print('diff ', np.min(diff))
-    diff = diff + - np.min(diff)
-    diff = diff / np.max(diff) * 255
-
-    print('diff ', np.max(diff))
-    print('diff ', np.min(diff))
-
-    cv2.imshow('diff', diff)
-    cv2.imshow('cv2', image_cv)
-    cv2.waitKey(0)
+    #cv2.imshow('diff', diff)
+    #cv2.imshow('cv2', image_cv)
+    #cv2.waitKey(0)
 
 
 
@@ -95,16 +118,16 @@ for i in glob.iglob('/Users/didi/Downloads/train/*.jpg'):
 #
 #
 
-transform_train = transforms.Compose([
-        transforms.ToFloat(), #可加可不加
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(),
-        transforms.Normalize(mean_train, std_train),
-        transforms.ToTensor(),
-    ])
-
-transform_test = transforms.Compose([
-        transforms.CenterCrop(),
-        transforms.Normalize(mean_train, std_train)
-])
+#transform_train = transforms.Compose([
+#        transforms.ToFloat(), #
+#        transforms.RandomResizedCrop(224),
+#        transforms.RandomHorizontalFlip(),
+#        transforms.ColorJitter(),
+#        transforms.Normalize(mean_train, std_train),
+#        transforms.ToTensor(),
+#    ])
+#
+#transform_test = transforms.Compose([
+#        transforms.CenterCrop(),
+#        transforms.Normalize(mean_train, std_train)
+#])
