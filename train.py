862fd6feb9c25add07f947777df4c0df8eedf003
weiaicunzai <372995411@qq.com>
2019-01-02 18:05:25 +0800

    transforms module complete

--- train.py
+++ train.py
@@ -3,18 +3,108 @@
 
 import numpy as np
 import cv2
+import torch
+import glob
+
+from PIL import Image
+from transforms import transforms
 
 
 path = '/Users/didi/Downloads/train/2d281959a02178bbcdeea424c8757b1d.jpg'
 
-image = cv2.imread(path)
-
-print(image.dtype)
-print(np.max(image))
-image = image.astype('float32')
-
-print(image.dtype)
-print(np.max(image))
+for i in glob.iglob('/Users/didi/Downloads/train/*.jpg'):
+    image_cv = cv2.imread(i)
+    #b, g, r = cv2.split(image_cv)
+    #image_cv = cv2.merge((r, g, b))
 
 
+    image_cv = cv2.resize(image_cv, (224, 224), interpolation=cv2.INTER_LINEAR)
+    print(image_cv.dtype)
 
+    image_pil = Image.open(i)
+    image_pil = image_pil.resize((224, 224), resample=Image.BILINEAR)
+
+    #print(image_cv.dtype)
+    #print(image_pil.dtype)
+
+    image_pil.show('pil')
+
+    image_pil = np.array(image_pil)
+    print(image_pil.dtype)
+
+    r, g, b = cv2.split(image_pil)
+    image_pil = cv2.merge((b, g, r))
+
+    print(np.mean(image_cv - image_pil))
+
+    diff = image_cv - image_pil
+    print('diff ', np.max(diff))
+    print('diff ', np.min(diff))
+    diff = diff + - np.min(diff)
+    diff = diff / np.max(diff) * 255
+
+    print('diff ', np.max(diff))
+    print('diff ', np.min(diff))
+
+    cv2.imshow('diff', diff)
+    cv2.imshow('cv2', image_cv)
+    cv2.waitKey(0)
+
+
+
+
+    #cv2.imshow('origin', image)
+    #trans = transforms.CenterCrop()
+    #image = trans(image)
+#trans = transforms.ToFloat()
+#image = trans(image)
+#print(image.dtype)
+    #trans = transforms.RandomResizedCrop(224)
+    #image = trans(image)
+#print(image.dtype)
+    #trans = transforms.RandomHorizontalFlip()
+    #image = trans(image)
+#print(image.dtype)
+    #trans = transforms.ColorJitter()
+    #image = trans(image)
+#print(image.dtype)
+    #trans = transforms.Normalize(mean=[0.7, 0.8, 0.3], std=[0.1, 0.2, 0.3])
+    #image = trans(image)
+#print(image.dtype)
+#trans = transforms.ToTensor()
+#image = trans(image)
+
+    #cv2.imshow('test', image)
+    #cv2.waitKey(0)
+#print(type(image))
+#print(image.size())
+#print(image.dtype)
+#print(torch.max(image))
+#cv2.imshow('test', image)
+#cv2.waitKey(0)
+#print(image.shape)
+#
+#print(image.dtype)
+#print(np.max(image))
+#image = image.astype('float32')
+#
+#print(image.dtype)
+#print(np.max(image))
+#
+#
+#
+#
+
+transform_train = transforms.Compose([
+        transforms.ToFloat(), #可加可不加
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(),
+        transforms.Normalize(mean_train, std_train),
+        transforms.ToTensor(),
+    ])
+
+transform_test = transforms.Compose([
+        transforms.CenterCrop(),
+        transforms.Normalize(mean_train, std_train)
+])
---
+++ transforms/transforms.py
@@ -0,0 +1,270 @@
+
+import random
+import math
+import numbers
+
+import cv2
+import numpy as np
+
+import torch
+
+
+class ToFloat:
+    """Convert an image data type to 32-bit floating point
+    """
+
+    def __call__(self, image):
+        """
+        Args:
+            image (numpy array): Image to be converted to 32-bit floating point
+        
+        Returns:
+            image (numpy array): Converted Image
+        """
+        return image.astype('float32')
+
+
+class RandomResizedCrop:
+    """Randomly crop a rectangle region whose aspect ratio is randomly sampled 
+    in [3/4, 4/3] and area randomly sampled in [8%, 100%], then resize the cropped
+    region into a 224-by-224 square image.
+
+    Args:
+        size: expected output size of each edge
+        scale: range of size of the origin size cropped
+        ratio: range of aspect ratio of the origin aspect ratio cropped (w / h)
+        interpolation: Default: cv2.INTER_LINEAR: 
+    """
+
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3.0 / 4.0, 4.0 / 3.0), interpolation='linear'):
+
+        self.methods={
+            "area":cv2.INTER_AREA, 
+            "nearest":cv2.INTER_NEAREST, 
+            "linear" : cv2.INTER_LINEAR, 
+            "cubic" : cv2.INTER_CUBIC, 
+            "lanczos4" : cv2.INTER_LANCZOS4
+        }
+
+        self.size = (size, size)
+        self.interpolation = self.methods[interpolation]
+        self.scale = scale
+        self.ratio = ratio
+
+    
+    def __call__(self, img):
+        h, w, _ = img.shape
+
+        area = w * h
+
+        for attempt in range(10):
+            target_area = random.uniform(*self.scale) * area
+            target_ratio = random.uniform(*self.ratio) 
+
+            output_h = int(round(math.sqrt(target_area * target_ratio)))
+            output_w = int(round(math.sqrt(target_area / target_ratio))) 
+
+            if random.random() < 0.5:
+                output_w, output_h = output_h, output_w 
+
+            if output_w <= w and output_h <= h:
+                topleft_x = random.randint(0, w - output_w)
+                topleft_y = random.randint(0, h - output_h)
+                break
+
+        if output_w > w or output_h > h:
+            output_w = min(w, h)
+            output_h = output_w
+            topleft_x = random.randint(0, w - output_w) 
+            topleft_y = random.randint(0, h - output_w)
+
+        cropped = img[topleft_y : topleft_y + output_h, topleft_x : topleft_x + output_w]
+
+        resized = cv2.resize(cropped, self.size, interpolation=self.interpolation)
+
+        return resized
+    
+    def __repr__(self):
+        for name, inter in self.methods.items():
+            if inter == self.interpolation:
+                inter_name = name
+
+        interpolate_str = inter_name
+        format_str = self.__class__.__name__ + '(size={0}'.format(self.size)
+        format_str += ', scale={0}'.format(tuple(round(s, 4) for s in self.scale))
+        format_str += ', ratio={0}'.format(tuple(round(r, 4) for r in self.ratio))
+        format_str += ', interpolation={0})'.format(interpolate_str)
+
+        return format_str
+
+
+class RandomHorizontalFlip:
+    """Horizontally flip the given opencv image with given probability p.
+
+    Args:
+        p: probability of the image being flipped
+    """
+    def __init__(self, p=0.5):
+        self.p = p
+    
+    def __call__(self, img):
+        """
+        Args:
+            the image to be flipped
+        Returns:
+            flipped image
+        """
+        if random.random() < self.p:
+            img = cv2.flip(img, 1)
+        
+        return img
+
+
+class ColorJitter:
+    """Scale hue, saturation, and brightness with coefficients uniformly
+    drawn from [0.6, 1.4]
+
+    Args:
+        h: range to scale hue
+        b: range to scale brightness
+        s: range to scale saturation
+    """
+
+    def __init__(self, h=[0.6, 1.4], b=[0.6, 1.4], s=[0.6, 1.4]):
+        self.h = h
+        self.b = b
+        self.s = s
+
+    def __call__(self, img):
+        """
+        Args:
+            img to be jittered
+        Returns:
+            jittered img
+        """
+
+        h_factor = random.uniform(*self.h)
+        b_factor = random.uniform(*self.b)
+        s_factor = random.uniform(*self.s)
+
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, b = cv2.split(img_hsv)
+
+        h = h * h_factor
+        b = b * b_factor
+        s = s * s_factor
+
+        #normalize
+        h = h / np.max(h) * 255.0
+        b = b / np.max(b) * 255.0
+        s = s / np.max(s) * 255.0
+
+        #convert data type
+        h = h.astype(img_hsv.dtype)
+        b = b.astype(img_hsv.dtype)
+        s = s.astype(img_hsv.dtype)
+
+        img_hsv = cv2.merge((h, s, b))
+        img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+
+        return img
+
+class ToTensor:
+    """convert an opencv image (h, w, c) ndarray to a pytorch float tensor 
+    (c, h, w) 
+    """
+
+    def __call__(self, img):
+        """
+        Args:
+            a numpy array (h, w, c) range from [0, 255]
+        
+        Returns:
+            a pytorch tensor
+        """
+        #convert format H W C to C H W
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img)
+        img = img.float()
+
+        return img
+
+
+class Normalize:
+    """Normalize a numpy array (H, W, BGR order) with mean and standard deviation
+    to float32 data type range from [0, 1]
+
+    for each channel in numpy array:
+        ``input[channel] = (input[channel] - mean[channel]) / std[channel]``
+
+    Args:
+        mean: sequence of means for each channel
+        std: sequence of stds for each channel
+    """
+
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+    
+    def __call__(self, img):
+        """
+        Args:
+            (H W C) format numpy array range from [0, 255]
+        Returns:
+            (H W C) format numpy array in float32 range from [0, 1]
+        """        
+        img = img / 255.0
+
+        for index, mean in enumerate(self.mean):
+            img[:, :, index] -= mean
+        
+        for index, std in enumerate(self.std):
+            img[:, :, index] /= std
+        
+        return img
+
+        
+class CenterCrop:
+    """resize each image’s shorter edge to r pixels while keeping its aspect ratio. 
+    Next, we crop out the cropped region in the center 
+    Args:
+        resized: resize image' shorter edge to resized pixels while keeping the aspect ratio
+        cropped: output image size(h, w), if cropped is an int, then output cropped * cropped size
+                 image
+    """
+
+    def __init__(self, resized=256, cropped=224, interpolation='linear'):
+
+        methods = {
+            "area":cv2.INTER_AREA, 
+            "nearest":cv2.INTER_NEAREST, 
+            "linear" : cv2.INTER_LINEAR, 
+            "cubic" : cv2.INTER_CUBIC, 
+            "lanczos4" : cv2.INTER_LANCZOS4
+        }
+        self.interpolation = methods[interpolation]
+
+        self.resized = resized
+
+        if isinstance(cropped, numbers.Number):
+            cropped = (cropped, cropped)
+        
+        self.cropped = cropped
+
+    def __call__(self, img):
+
+        shorter = min(*img.shape[:2])
+
+        scaler = float(self.resized) / shorter
+
+        img = cv2.resize(img, (0, 0), fx=scaler, fy=scaler, interpolation=self.interpolation)
+
+        h, w, _ = img.shape
+
+        topleft_x = int((w - self.cropped[1]) / 2)
+        topleft_y = int((h - self.cropped[0]) / 2)
+
+        center_cropped = img[topleft_y : topleft_y + self.cropped[0], 
+                             topleft_x : topleft_x + self.cropped[1]]
+
+        return center_cropped
